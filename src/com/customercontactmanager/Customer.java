c0559dd2372d72d0cda23dd769dbb3621c114759
josh <jallgood4386@gmail.com>
2026-09-12 14:34:26 -0400

    Create Customer model with contact fields and accessors.

---
+++ src/com/customercontactmanager/Customer.java
@@ -0,0 +1,36 @@
+package com.customercontactmanager;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public Customer(int id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public String toString() {
+        return id + " | " + name + " | " + email + " | " + phone;
+    }
+}
