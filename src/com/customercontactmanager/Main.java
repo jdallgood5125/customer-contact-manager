566ab52402be1abbc12cd9be6b0112184e897279
josh <jallgood4386@gmail.com>
2026-09-13 18:05:44 -0400

    Add interactive customer menu.

--- src/com/customercontactmanager/Main.java
+++ src/com/customercontactmanager/Main.java
@@ -1,32 +1,51 @@
 package com.customercontactmanager;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         CustomerManager manager = new CustomerManager();
 
-        manager.addCustomer(new Customer(1, "John Smith", "john.smith@email.com", "555-0101"));
+        manager.addCustomer(new Customer( 1, "John Smith", "john.smith@email.com", "555-0101"));
 
-        manager.addCustomer(new Customer(2, "Jane Doe", "jane.doe@email.com", "555-0102"));
+        manager.addCustomer(new Customer(2, "Jane Doe", "jane.doe@email", "555-0102"));
 
-        System.out.println("All customers:");
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
 
-        for (Customer customer : manager.getCustomers()) {
-            System.out.println(customer);
+        while (running) {
+            System.out.println("\n Customer Manager");
+
+            System.out.println("1. List Customers");
+            System.out.println("2. Find Customer");
+            System.out.println("3. Exit");
+            System.out.print("Choose and option: ");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    for (Customer customer : manager.getCustomers()) {
+                        System.out.println(customer);
+                    }
+                    break;
+
+                case "2":
+                    System.out.print("Enter Customer ID: ");
+                    int id = Integer.parseInt(scanner.nextLine());
+
+                    System.out.println(manager.findCustomerById(id));
+                    break;
+
+                case "3":
+                    running = false;
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid option.");
+            }
         }
 
-        System.out.println("\nUpdating customer with ID 1:");
-
-        boolean updated = manager.updateCustomer(
-                1,
-                "John Smith",
-                "john.new@email.com",
-                "555-0199"
-        );
-
-        if (updated) {
-            System.out.println(manager.findCustomerById(1));
-        } else {
-            System.out.println("Customer not found.");
-        }
+        scanner.close();
     }
 }
